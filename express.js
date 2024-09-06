a0091040f7cc1662dc15350e4b7989262b52387a
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-09-06 14:08:06 +0700

    feat: it is the time

---
+++ express.js
@@ -0,0 +1,33 @@
+const $btnYes = document.querySelector('.js-yes');
+const $btnNo = document.querySelector('.js-no');
+const $confirm = document.querySelector('.js-confirm');
+const $dialog = document.querySelector('.js-dialog');
+const $body = document.querySelector('body');
+
+let counter = 0;
+
+
+$btnYes.addEventListener('mouseover', (e) => {
+    e.target.innerHTML = 'Con cặc'
+})
+
+$btnYes.addEventListener('mouseout', (e) => {
+    e.target.innerHTML = 'YES'
+})
+
+$btnYes.addEventListener('click', () => {
+    $body.classList.add('yes')
+})
+
+$btnNo.addEventListener('mouseover', (e) => {
+    const $target = e.target;
+
+    $target.style.top = `${Math.random() * 200}px`;
+    $target.style.right = `${Math.random() * 200}px`;
+
+    if (counter >= 5) {
+        $target.innerHTML = 'Đừng đuổi em'
+    }
+
+    counter += 1;
+})
